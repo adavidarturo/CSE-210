d71b9df8d61610179e03c60a8fae3df4b750b809
adavidarturo <adavidarturo9@gmail.com>
2024-10-31 18:58:39 -0500

    Ask for your grade programm and display a letter

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Type your note: ");
+        string userInput = Console.ReadLine();
+        float number = int.Parse(userInput);
+        
+        if (number >= 90) 
+        {
+            Console.WriteLine("Your grade is: A ");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("Your grade is: B ");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("Your grade is: C ");
+            Console.WriteLine("You passed the course! ");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("Your grade is: D ");
+            Console.WriteLine("You will do it better next time ");
+        }
+        else if (number < 60)
+        {
+            Console.WriteLine("Your grade is: F ");
+            Console.WriteLine("You will do it better next time ");
+        }
+
     }
 }
