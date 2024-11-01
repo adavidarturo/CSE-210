6dd6565d2e18fac5e9a600c8165758985db9fe35
adavidarturo <adavidarturo9@gmail.com>
2024-10-31 22:40:09 -0500

    Guess the magic number programm

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+        int attempts = 0;
+
+        while (magicNumber != guess)
+        {
+            Console.Write("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+
+                if (magicNumber < guess)
+                 {
+                     Console.WriteLine("Lower");
+                 }
+
+                if (magicNumber > guess)
+                 {
+                     Console.WriteLine("Higher");
+                 }
+        }
+        Console.WriteLine($"Congratulation it took you {attempts} attempts");
     }
 }
