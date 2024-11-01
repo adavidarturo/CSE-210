6b3d51bdb09b8fdefe7c70eee882ee2a00925e21
adavidarturo <adavidarturo9@gmail.com>
2024-11-01 17:39:23 -0500

    Ask the user to add numbers till it is 0

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int addItem = -1;
+        List <int> numbersList = new List<int>();
+
+        Console.WriteLine("Enter alist of numbers, type 0 when finished. ");
+
+        while (addItem != 0)
+        {
+            Console.Write("Enter a number: ");
+            addItem = int.Parse(Console.ReadLine());
+            numbersList.Add(addItem);
+        }
+    
     }
+
 }
