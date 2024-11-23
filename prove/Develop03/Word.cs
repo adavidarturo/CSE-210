ef1c303a28b2db5be9952d1396afc0ce1d1e75ab
adavidarturo <adavidarturo9@gmail.com>
2024-11-22 20:10:48 -0500

    Commets added

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,38 +1,46 @@
 public class Word
 {
-    private string _text;
-    private bool _isHidden;
+    // Declare 2 characteristics
+    private string _text; // To storage text
+    private bool _isHidden; // To decide if it is hidden or not 
 
+    // Constructor to initialize characteristics 
     public Word(string text)
     {
         _text = text;
-        _isHidden = false;
+        _isHidden = false; // Default False value
     }
 
-    public void Hide()
+    // Method to Hide word
+    public void Hide() 
     {
         _isHidden = true;
     }
 
+    // Method to Show word
     public void Show()
     {
         _isHidden = false;
     }
 
+    // Method to storage the decision 
     public bool IsHidden()
     {
         return _isHidden;
     }
     
+    // Method to Get the final Display in _text and "_"
     public string GetDisplayText()
     {
-        if (_isHidden)
+        if (_isHidden) // If _isHidden is True 
         {
-            return new string('_', _text.Length);
+            // Save a new instance from the _text length in "_"
+            return new string('_', _text.Length); 
         }
         else
         {
-            return _text;
+            // Save only the _text
+            return _text; 
         }
     }
 }
