1b42100e96419a3ce4e5ec6fd64a26ddac8c4494
adavidarturo <adavidarturo9@gmail.com>
2024-11-30 19:29:55 -0500

    Custumer class

---
+++ foundation/Foundation2/Custumer.cs
@@ -0,0 +1,33 @@
+public class Customer
+{
+    private string _name;
+    private Address _address; // Person's full address
+
+    // Constructor to initialize variables
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+
+    // Method to get the person's name
+    public string GetName()
+    {
+        return _name;
+    }
+
+    // Method to get the person's full address
+    public Address GetAddress()
+    {
+        return _address;
+    }
+
+    // Method to verify if the person lives in USA
+    public bool IsInUSA()
+    {
+        return _address.IsInUSA(); // Save the IsInUSA() method from "Address"
+    }
+}
+
+
+
