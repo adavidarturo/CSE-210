8b2dfc2a32030a256eaa7f38559bb33aa54ba6a8
adavidarturo <adavidarturo9@gmail.com>
2024-12-01 19:05:05 -0500

    Use only 1 constructor

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -4,19 +4,10 @@ public class Reference
     private string _book; // To storage the _book name
     private int _chapter; // To storage the _chapter 
     private int _verse; // To storage the _verse
-    private int _endVerse; // To storage the final verse
+    private int? _endVerse; // To storage the final verse
 
-    // Constructor to initialize one verse
-    public Reference(string book, int chapter, int verse)
-    {
-        _book = book; 
-        _chapter = chapter;
-        _verse = verse;
-        _endVerse = -1; // No range 
-    }
-
-    // Constructor to initialize a range 
-    public Reference(string book, int chapter, int startVerse, int endVerse)
+    // Constructor to initialize variables
+    public Reference(string book, int chapter, int startVerse, int? endVerse = null)
     {
         _book = book;
         _chapter = chapter;
@@ -27,7 +18,7 @@ public class Reference
     // Method to Display the References
     public string GetDisplayText()
     {
-        if (_endVerse == -1) // If _endVerse is equal to -1
+        if (_endVerse.HasValue) // If _endVerse is equal to -1
         {
             return $"{_book} {_chapter}:{_verse}"; // Save book, chapter and verse
         }
