9594a4c6fe64ea01ab67a80cd688e1e174ee7b3f
adavidarturo <adavidarturo9@gmail.com>
2024-12-06 23:40:36 -0500

    Listing Activity class finished

---
+++ prove/Develop05/ListingActivity.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+
+public class ListingActivity : Activity
+{
+    private List<string> _prompts;
+
+    // Initialize list with prompts
+    public ListingActivity() : base("Listing", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area")
+    {
+        _prompts = new List<string>
+        {
+            "\n --- Who are people that you appreciate?\n",
+            "\n --- What are personal strengths of yours?\n",
+            "\n --- Who are people that you have helped this week?\n",
+            "\n --- When have you felt the Holy Spirit this month?\n",
+            "\n --- Who are some of your personal heroes?\n"
+        };
+    }
+
+    // Method to get a random prompt
+    private string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+
+    // Method to get a written list of from user
+    private List<string> GetListFromUser()
+    {
+        List<string> items = new List<string>(); // New "items" list
+        while (true)
+        {
+            Console.Write("> "); // Prompt
+            string item = Console.ReadLine(); // Optain "item" strings
+            if (string.IsNullOrEmpty(item)) break; // If there is nothing in the string, "break" 
+            items.Add(item); // Add each item to the "items" list
+        }
+        return items; // Save "items" list
+    }
+    
+    // Method to Run all Listing Activity methods 
+    public override void Run()
+    {
+        DisplayStartingMessage();
+        Console.WriteLine("List as many responses you can to the following prompt:");
+        Console.WriteLine(GetRandomPrompt());
+        Console.WriteLine("You may begin in: ");
+        ShowCountDown(5);
+        List<string> items = GetListFromUser();
+        Console.WriteLine($"\nYou listed {items.Count} items!\n"); // Display "items" count
+        DisplayEndingMessage();
+    }
+}
+
+
